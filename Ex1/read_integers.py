51663246fa868b969d9d0b5cbd3c96e29c68b01d
Minh Nhat <minhnhat44.fr@gmail.com>
2024-01-15 14:26:10 +0200

    Task 5: Add a function to the previous task that counts the number of even integers that were among the entered

--- Ex1/read_integers.py
+++ Ex1/read_integers.py
@@ -1,6 +1,7 @@
 number=''
 list_numbers=[]
 list_negav=[]
+list_even_numbers=[]
 
 
 while number !=0:
@@ -15,4 +16,11 @@ def negative_integer(list_numbers):
             list_negav.append(number)
     return len(list_negav)
 
+def even_numbers(list_numbers):
+    for number in list_numbers:
+        if number%2==0:
+           list_even_numbers.append(number) 
+    return len(list_even_numbers)
+  
+print('Number of even numbers is', even_numbers(list_numbers))   
   
