a2f18572a9caee22555acad0259bc83a103d1276
Minh Nhat <minhnhat44.fr@gmail.com>
2024-01-15 14:19:39 +0200

    Task 3: Arrange numbers and strings in required orders

--- Ex1/list_items.py
+++ Ex1/list_items.py
@@ -1,19 +1,22 @@
 import random
 list_numbers=[]
 for i in range(10): 
-    # # number input from user 
-    # number=int(input('Please type in a number: '))
-
-    # number input randomly
-    number=random.randrange(-(2**31), 2**31)
+    # number input from user 
+    number=int(input('Please type in a number: '))
+    # # number input randomly
+    # number=random.randrange(-(2**31), 2**31)
     list_numbers.append(number)
-    # list_numbers.sort()
+
+    # Arrange numbers in the list from smallest to largest
+    list_numbers.sort()
 
 print('List of numbers', list_numbers)
 
 list_strings=[]
 for j in range(10):
     string=input('Please type in a string: ')
-    list_strings.append(string)  
- # list_strings.sort()
+    list_strings.append(string) 
+    
+    # Arrange strings in alphabetical order  
+    list_strings.sort()
 print('List of strings', list_strings)
