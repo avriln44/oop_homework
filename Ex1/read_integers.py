9fc5580b1b4a5b47337c26f62f955108c049b3f8
Minh Nhat <minhnhat44.fr@gmail.com>
2024-01-15 14:27:05 +0200

    Task 6: Add  a function that counts the sum of the positive integers divisible by three.

--- Ex1/read_integers.py
+++ Ex1/read_integers.py
@@ -2,7 +2,7 @@ number=''
 list_numbers=[]
 list_negav=[]
 list_even_numbers=[]
-
+list_positive_numbers=[]
 
 while number !=0:
     number=int(input('Please type in an integer: '))
@@ -16,6 +16,7 @@ def negative_integer(list_numbers):
             list_negav.append(number)
     return len(list_negav)
 
+
 def even_numbers(list_numbers):
     for number in list_numbers:
         if number%2==0:
@@ -23,4 +24,11 @@ def even_numbers(list_numbers):
     return len(list_even_numbers)
   
 print('Number of even numbers is', even_numbers(list_numbers))   
+
+def sum_positive_numbers(list_numbers):
+    for number in list_numbers:
+        if number%3==0 and number>0:
+           list_positive_numbers.append(number) 
+    return sum(list_positive_numbers)
   
+print('Sum of positive numbers which is divisible by 3 is', sum_positive_numbers(list_numbers))   
